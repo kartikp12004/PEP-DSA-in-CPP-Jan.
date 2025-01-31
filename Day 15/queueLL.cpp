ca145d3e0ffb60a22935db2dc346976de8983be7
Kartik Pandey <karthikpandey580@gmail.com>
2025-01-31 17:18:52 +0530

    Create queueLL.cpp

---
+++ Day 15/queueLL.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class LLnode{
+    public:
+    int val;
+    LLnode*next;
+    LLnode(int val){
+        this->val = val;
+        this->next = nullptr;
+    }
+};
+
+class Queue{
+    LLnode*h;
+    public:
+};
