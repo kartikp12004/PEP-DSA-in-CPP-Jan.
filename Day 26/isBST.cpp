93dc2aa6c7b5342a11dcaba82557c9612a7d5832
Kartik Pandey <karthikpandey580@gmail.com>
2025-02-21 22:07:15 +0530

    Create isBST.cpp

---
+++ Day 26/isBST.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class BTnode{
+    public:
+    int val;
+    BTnode*left;
+    BTnode*right;
+    BTnode(int val){
+        this->val = val;
+    }
+};
+
+bool isBST(BTnode*root){
+
+}
